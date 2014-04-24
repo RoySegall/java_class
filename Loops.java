17855196ef0755f950102667c3475f56660efa7c
RoySegall <roy.segall@gizra.com>
2014-04-24 19:04:48 +0300

    Add Loops

--- Loops.java
+++ Loops.java
@@ -3,20 +3,18 @@ import java.lang.String;import java.lang.System;import java.util.Scanner;
 public class Loops {
 
   public static void main(String[] args) {
-    int number;
+    int number, max = 0;
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a number, -1 to quit.");
-    number = scan.nextInt();
-    int max = number;
 
-    while (number != -1) {
+    do {
+      System.out.println("Enter number, -1 to quit");
+      number = scan.nextInt();
+
       if (number > max) {
         max = number;
       }
 
-      System.out.println("Enter number, -1 to quit");
-      number = scan.nextInt();
-    }
+    } while (number != -1);
 
     System.out.println("The bigger number you enter is: " + max);
   }
